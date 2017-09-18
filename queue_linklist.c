54cb2e16ebf2b83657d33e0b61dda22852c35c25
jackmail <peterpainkiller@gmail.com>
2017-09-18 15:34:12 +0800

    上传链式队列代码

---
+++ queue_linklist.c
@@ -0,0 +1,91 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int ElementType;
+
+struct Node{
+   ElementType Data;
+   struct Node *Next;
+};
+
+typedef struct Node *PtrToNode;
+typedef PtrToNode Position;
+
+struct QNode{
+   Position front;
+   Position rear;
+   int MaxSize;
+};
+
+typedef struct QNode *Queue;
+
+int IsEmpty(Queue Q)
+{
+    if(NULL == Q->front)
+    {
+	return TRUE;
+    }
+    else
+    {
+	return FALSE;
+    }
+}
+
+ElementType DeleteQ(Queue Q)
+{
+   Position frontCell;
+   ElementType frontElement;
+
+   if(IsEmpty(Q))
+   {
+      printf("queue empty");
+      return -1;
+   }
+   else
+   {
+	frontCell = Q->front;
+        if(Q->front == Q->Rear)
+        {
+	     Q->front = Q->rear = NULL;
+        }
+        else
+        {
+	     Q->front = Q->front->Next;
+        }
+        frontElement = frontCell->Data;
+        free(frontCell);
+        return frontElement;
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
