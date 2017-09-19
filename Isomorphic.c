eed3838086bfbd5d8ab9bbf7b3bbb41bc72d43d4
jackmail <peterpainkiller@gmail.com>
2017-09-19 15:42:31 +0800

    判断是否是同构树，这个解法只得了13分

--- Isomorphic.c
+++ Isomorphic.c
@@ -1,1 +1,59 @@
-
+int Isomorphic( Tree T1, Tree T2 )
+{
+  int ret1 = 0;
+  int ret2 = 0;
+  
+  if(NULL == T1 && NULL  == T2)
+  {
+    return 1;
+  }
+  else 
+  {
+    return 0;
+  }
+  
+  if(T1 != T2)
+  {
+    return 0;
+  }
+  
+  if(T1->Left != T2->Left && T1->Right != T2->Right && T1->Left != T2->Right
+  && T1->Right != T2->Left)
+  {
+     return 0;
+  }
+  if((T1->Left == T2->Left && T1->Right != T2->Right) || (T1->Right == T2->Right &&
+  T1->Left != T2->Left))
+  {
+    return 0;
+  }
+ 
+  if(T1->Left == T2->Left && T1->Right == T2->Right)
+  {
+    ret1 = Isomorphic(T1->Left,T2->Left);
+    ret2 = Isomorphic(T2->Right,T2->Right);
+    if(ret1 ==1 && ret2 ==1)
+    {
+       return 1;
+    }
+    else
+    {
+       return 0;
+    }
+  }
+  
+  if(T1->Left == T2->Right && T1->Right == T2->Left)
+  {
+    ret1 = Isomorphic(T1->Left,T2->Right);
+    ret2 = Isomorphic(T1->Right,T2->Left);
+    if(ret1 == 1 && ret2 == 1)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  return 0;
+}
